8ab5f84937ce2c89b0199701de565e28da561d9e
Peter M. Reynolds <txpmr1@gmail.com>
2017-09-01 22:05:58 -0500

    feat: Introduce a test error using the allFeeds variable
    6. Edit the allFeeds variable in ./js/app.js to make the provided test fail and see how Jasmine visualizes this failure in your application.

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@
  */
 
 // The names and URLs to all of the feeds we'd like available.
-var allFeeds = [
-    {
+var allFeeds = [];
+/*  {
         name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
     }, {
@@ -21,7 +21,7 @@ var allFeeds = [
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
     }
-];
+]; */
 
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
