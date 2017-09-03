b08e89b3473729300721b71202b033de7c545865
Peter M. Reynolds <txpmr1@gmail.com>
2017-09-03 14:28:18 -0500

    feat: Add tests to ensure that each feed has a url and a name
    8. Write a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
    
    9. Write a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,19 +26,28 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('each feed has a url', function(){
+          allFeeds.forEach(function(feed){
+            expect(feed.url).toBeDefined();
+            expect(feed.url.length).not.toBe(0);
+          });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each feed has a name', function(){
+          allFeeds.forEach(function(feed){
+            expect(feed.name).toBeDefined();
+            expect(feed.name.length).not.toBe(0);
+          });
+        });
     });
-
 
     /* TODO: Write a new test suite named "The menu" */
 
