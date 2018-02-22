82b982c058fd8f953d2fb055e4030f41e67d4b2b
Peter M. Reynolds <txpmr1@gmail.com>
2018-02-22 14:30:36 -0600

    fix: remove nested structure and feed tests from inital entries and feed selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,111 +67,54 @@ $(function() {
      */
     describe('Initial Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+        /* Load feed 0 asynchronously before checking for entries.
          */
-
-        /* Test to ensure that feed 0 is fully defined before loading
-         * with the beforeEach function.
-         */
-        it('feed 0 is defined with a name and url', function() {
-            expect(allFeeds[0]).toBeDefined();
-            expect(allFeeds[0].url).toBeDefined();
-            expect(allFeeds[0].url.length).not.toBe(0);
-            expect(allFeeds[0].name).toBeDefined();
-            expect(allFeeds[0].name.length).not.toBe(0);
+        beforeEach(function(done) {
+          loadFeed(0, function() {
+            done();
+          });
         });
 
-        /* Nest feed 0 entry tests to ensure that loadFeed does not attempt
-         * to load an undefined feed.
+        /* Test to ensure that loadFeed created entries.
          */
-        describe('feed 0 entries', function() {
-
-            /* Load feed 0 asynchronously before checking for entries.
-             */
-            beforeEach(function(done) {
-              loadFeed(0, function() {
-                done();
-              });
-            });
-    
-            /* Test to ensure that loadFeed created entries.
-             */
-            it('are loaded', function(){
-              var numEntryLinks = $(".feed .entry-link").length;
-              expect(numEntryLinks).not.toBe(0);
-            });
+        it('entries are loaded', function(){
+          var numEntries = $(".feed .entry").length;
+          expect(numEntries).not.toBe(0);
         });
     });
 
     /* Check feed selection functionality.
      */
     describe('New Feed Selection', function() {
-        var links0, links1;
-        
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        var links0, links1; // to save asynchronously loaded entry links
 
-        /* Test to ensure that feed 0 is fully defined before loading
-         * with the beforeEach function.
+        /* Asynchronously load feed 0 followed by feed 1 and save the entry links
+         * for both feeds.
          */
-        it('feed 0 is defined with a name and url', function() {
-            expect(allFeeds[0]).toBeDefined();
-            expect(allFeeds[0].url).toBeDefined();
-            expect(allFeeds[0].url.length).not.toBe(0);
-            expect(allFeeds[0].name).toBeDefined();
-            expect(allFeeds[0].name.length).not.toBe(0);
-        });
-
-        /* Test to ensure that feed 1 is fully defined before loading
-         * with the beforeEach function.
-         */
-        it('feed 1 is defined with a name and url', function() {
-            expect(allFeeds[1]).toBeDefined();
-            expect(allFeeds[1].url).toBeDefined();
-            expect(allFeeds[1].url.length).not.toBe(0);
-            expect(allFeeds[1].name).toBeDefined();
-            expect(allFeeds[1].name.length).not.toBe(0);
-        });
-
-        /* Nest feed 0 and feed 1 entry tests to ensure that loadFeed does not
-         * attempt to load an undefined feed.
-         */
-        describe('loading feed 0 followed by loading feed 1', function() {
-
-            /* Asynchronously load feed 0 followed by feed 1 and save the entries
-             * for both feeds.
-             */
-            beforeEach(function(done) {
-              links0 = [];
-              links1 = [];
-              loadFeed(0, function() {
-                $(".feed .entry-link").each(function() {
-                  links0.push($(this).attr('href'));
-                });
-                loadFeed(1, function() {
-                  $(".feed .entry-link").each(function() {
-                    links1.push($(this).attr('href'));
-                  });
-                  done();
-                });
+        beforeEach(function(done) {
+          links0 = [];
+          links1 = [];
+          loadFeed(0, function() {
+            $(".feed .entry-link").each(function() {
+              links0.push($(this).attr('href'));
+            });
+            loadFeed(1, function() {
+              $(".feed .entry-link").each(function() {
+                links1.push($(this).attr('href'));
               });
+              done();
             });
+          });
+        });
     
-            /* Test to ensure that loading a new feed changes entries by
-             * comparing the saved entries for feed 0 and feed 1
-             */ 
-            it('changes feed content', function() {
-              // log both entry arrays here to verify beforeEach functionality
-              // console.log('links0 = ', links0);
-              // console.log('links1 = ', links1);
-              expect(links1).not.toEqual(links0);
-            });
+        /* Test to ensure that loading a new feed changes entries by
+         * comparing the saved entry links for feed 0 and feed 1
+         */ 
+        it('selection changes feed content', function() {
+          // log both entry arrays here to verify beforeEach functionality
+          // console.log('links0 = ', links0);
+          // console.log('links1 = ', links1);
+          expect(links1).not.toEqual(links0);
         });
     });
 }());
